b5d7f350200d89f6dec0e66e19fc7f32614b67e1
lawkawai <ddlljiawei@163.com>
2024-11-21 02:37:22 +0800

    success!!

--- SNES_heat.cpp
+++ SNES_heat.cpp
@@ -527,7 +527,7 @@ int main(int argc, char **args) {
     /*
     Set Jacobian matrix data structure and Jacobian evaluation routine
     */
-    // ierr = SNESSetJacobian(snes,J,J,FormJacobian,&user);CHKERRQ(ierr);
+    ierr = SNESSetJacobian(snes,J,J,FormJacobian,&user);CHKERRQ(ierr);
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     Customize nonlinear solver; set runtime options
@@ -617,24 +617,91 @@ PetscErrorCode FormFunction(SNES snes,Vec x,Vec f,void *ctx)
             the array.
     */
     ierr = VecGetArrayRead(x,&xx);CHKERRQ(ierr);
-    // ierr = VecGetArray(f,&F);CHKERRQ(ierr);
+    ierr = VecGetArray(f,&F);CHKERRQ(ierr);
 
-    AssemblyResidual(xx, *user);
+    // AssemblyResidual(xx, *user);
+
+    // Assembly stiffness matrix and load vector
+    for(int ii = 0; ii < user->n_eq; ++ii){
+        user->uh[ii] = xx[ii];
+    }
+
+    for (int ee = 0; ee < user->nElem; ++ee) {
+        // Allocate zero element stiffness matrix 
+        // std::vector<double> k_ele(user->n_en * user->n_en, 0.0);
+        std::vector<double> f_ele(user->n_en, 0.0);
+
+        std::vector<double> x_ele(user->n_en);
+        std::vector<double> d_ele(user->n_en);
+        std::vector<double> u_ele(user->n_en);
+        for (int aa = 0; aa < user->n_en; ++aa) {
+            x_ele[aa] = user->x_coor[user->IEN[ee + aa * user->nElem] - 1];
+            u_ele[aa] = user->uh[user->IEN[ee + aa * user->nElem] - 1];
+        }
+
+        // loop over quadrature points
+        for (int ll = 0; ll < user->nqp; ++ll) {
+            // we need the geometric mapping at each quadrature points
+            double x_qua = 0.0;
+            double dx_dxi = 0.0;
+            double u_qua = 0.0;
+            double du_dxi = 0.0;
+            for (int aa = 0; aa < user->n_en; ++aa) {
+                double Na = 0.0;
+                PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
+                x_qua += x_ele[aa] * Na;
+                u_qua += u_ele[aa] * Na;
+                double Na_xi = 0.0;
+                PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
+                dx_dxi += x_ele[aa] * Na_xi;
+                du_dxi += u_ele[aa] * Na_xi;
+            }
+            double dxi_dx = 1.0 / dx_dxi;
+
+            double kappa = fun_kappa( u_qua );
+            double dkappa = fun_dkappa( u_qua );
+
+            double detJ = dx_dxi;
+            // we loop over a and b to assemble the element stiffness matrix and load vector
+            for (int aa = 0; aa < user->n_en; ++aa){
+                double Na = 0.0;
+                PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
+                double Na_xi = 0.0;
+                PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
+
+                f_ele[aa] += user->wq[ll] * Na * f_body(x_qua) * detJ;
+                f_ele[aa] -= user->wq[ll] * Na_xi * kappa * du_dxi * dxi_dx;
+
+            }
+        }
+        // global assembly
+        // distribute the entries to the global stiffness matrix and global load vector
+        for (int aa = 0; aa < user->n_en; ++aa){
+            int PP = user->ID[ user->IEN[aa * user->nElem + ee] - 1 ];
+            if (PP > 0){
+                F[PP - 1] += f_ele[aa];
+            }
+        }
+        if (ee == 0){
+            F[ user->ID[ user->IEN[0] - 1 ] - 1 ] += h( user->x_coor[ user->IEN[0] - 1 ]);
+        }
+        
+    } //  end of element loop
+
+    for (int ii = 0; ii < user->n_eq; ++ii){
+        F[ii] = -F[ii];
+    }
     
     std::cout << "F: \n";
     for (int ii = 0; ii < user->n_eq; ++ii){
-        std::cout << user->F[ii] << "\t";
+        std::cout << F[ii] << "\t";
     }
     std::cout << std::endl;
-
-    for (PetscInt ii = 0; ii < user->n_eq; ++ii){
-        PetscCall(VecSetValues(f, 1, &ii, &user->F[ii], INSERT_VALUES));
-    }
 
 
     /* Restore vectors */
     ierr = VecRestoreArrayRead(x,&xx);CHKERRQ(ierr);
-    // ierr = VecRestoreArray(f,&F);CHKERRQ(ierr);
+    ierr = VecRestoreArray(f,&F);CHKERRQ(ierr);
 
 
     // ierr = VecView(f,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
@@ -661,7 +728,7 @@ PetscErrorCode FormJacobian(SNES snes,Vec x,Mat jac,Mat B,void *ctx)
 {
     AppCtx  *user = (AppCtx *)ctx;
     const PetscScalar *xx;
-    PetscScalar       *K;
+    PetscScalar       A[100];
     PetscErrorCode    ierr;
     //   PetscInt          idx[2] = {0,1};
 
@@ -671,106 +738,98 @@ PetscErrorCode FormJacobian(SNES snes,Vec x,Mat jac,Mat B,void *ctx)
     */
     ierr = VecGetArrayRead(x,&xx);CHKERRQ(ierr);
 
+    std::vector<double> K(user->n_eq * user->n_eq, 0.0);
+
     /*
         Compute Jacobian entries and insert into matrix.
         - Since this is such a small problem, we set all entries for
             the matrix at once.
     */
-    AssemblyJacobian(xx, *user);
+    // AssemblyJacobian(xx, *user);
     // Assembly stiffness matrix and load vector
-    // for(int ii = 0; ii < user->n_eq; ++ii){
-    //     user->uh[ii] = xx[ii];
-    // }
+    for(int ii = 0; ii < user->n_eq; ++ii){
+        user->uh[ii] = xx[ii];
+    }
 
-    // for (int ee = 0; ee < user->nElem; ++ee) {
-    //     // Allocate zero element stiffness matrix 
-    //     std::vector<double> k_ele(user->n_en * user->n_en, 0.0);
-    //     // std::vector<double> f_ele(user->n_en, 0.0);
+    for (int ee = 0; ee < user->nElem; ++ee) {
+        // Allocate zero element stiffness matrix 
+        std::vector<double> k_ele(user->n_en * user->n_en, 0.0);
+        // std::vector<double> f_ele(user->n_en, 0.0);
 
-    //     std::vector<double> x_ele(user->n_en);
-    //     std::vector<double> d_ele(user->n_en);
-    //     std::vector<double> u_ele(user->n_en);
-    //     for (int aa = 0; aa < user->n_en; ++aa) {
-    //         x_ele[aa] = user->x_coor[user->IEN[ee + aa * user->nElem] - 1];
-    //         u_ele[aa] = user->uh[user->IEN[ee + aa * user->nElem] - 1];
-    //     }
+        std::vector<double> x_ele(user->n_en);
+        std::vector<double> d_ele(user->n_en);
+        std::vector<double> u_ele(user->n_en);
+        for (int aa = 0; aa < user->n_en; ++aa) {
+            x_ele[aa] = user->x_coor[user->IEN[ee + aa * user->nElem] - 1];
+            u_ele[aa] = user->uh[user->IEN[ee + aa * user->nElem] - 1];
+        }
 
-    //     // loop over quadrature points
-    //     for (int ll = 0; ll < user->nqp; ++ll) {
-    //         // we need the geometric mapping at each quadrature points
-    //         double x_qua = 0.0;
-    //         double dx_dxi = 0.0;
-    //         double u_qua = 0.0;
-    //         double du_dxi = 0.0;
-    //         for (int aa = 0; aa < user->n_en; ++aa) {
-    //             double Na = 0.0;
-    //             PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
-    //             x_qua += x_ele[aa] * Na;
-    //             u_qua += u_ele[aa] * Na;
-    //             double Na_xi = 0.0;
-    //             PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
-    //             dx_dxi += x_ele[aa] * Na_xi;
-    //             du_dxi += u_ele[aa] * Na_xi;
-    //         }
-    //         double dxi_dx = 1.0 / dx_dxi;
+        // loop over quadrature points
+        for (int ll = 0; ll < user->nqp; ++ll) {
+            // we need the geometric mapping at each quadrature points
+            double x_qua = 0.0;
+            double dx_dxi = 0.0;
+            double u_qua = 0.0;
+            double du_dxi = 0.0;
+            for (int aa = 0; aa < user->n_en; ++aa) {
+                double Na = 0.0;
+                PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
+                x_qua += x_ele[aa] * Na;
+                u_qua += u_ele[aa] * Na;
+                double Na_xi = 0.0;
+                PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
+                dx_dxi += x_ele[aa] * Na_xi;
+                du_dxi += u_ele[aa] * Na_xi;
+            }
+            double dxi_dx = 1.0 / dx_dxi;
 
-    //         double kappa = fun_kappa( u_qua );
-    //         double dkappa = fun_dkappa( u_qua );
+            double kappa = fun_kappa( u_qua );
+            double dkappa = fun_dkappa( u_qua );
 
-    //         double detJ = dx_dxi;
-    //         // we loop over a and b to assemble the element stiffness matrix and load vector
-    //         for (int aa = 0; aa < user->n_en; ++aa){
-    //             double Na = 0.0;
-    //             PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
-    //             double Na_xi = 0.0;
-    //             PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
+            double detJ = dx_dxi;
+            // we loop over a and b to assemble the element stiffness matrix and load vector
+            for (int aa = 0; aa < user->n_en; ++aa){
+                double Na = 0.0;
+                PolyBasis(user->pp, aa + 1, 0, user->qp[ll], Na);
+                double Na_xi = 0.0;
+                PolyBasis(user->pp, aa + 1, 1, user->qp[ll], Na_xi);
 
-    //             // f_ele[aa] += user->wq[ll] * Na * f_body(x_qua) * detJ;
-    //             // f_ele[aa] -= user->wq[ll] * Na_xi * kappa * du_dxi * dxi_dx;
-
-    //             for (int bb = 0; bb < user->n_en; ++bb){
-    //                 double Nb = 0.0;
-    //                 PolyBasis(user->pp, bb + 1, 0, user->qp[ll], Nb);
-    //                 double Nb_xi = 0.0;
-    //                 PolyBasis(user->pp, bb + 1, 1, user->qp[ll], Nb_xi);
-    //                 k_ele[aa * user->n_en + bb] += user->wq[ll] * Na_xi * kappa * Nb_xi * dxi_dx;
-    //                 k_ele[aa * user->n_en + bb] += user->wq[ll] * Na_xi * dkappa * Nb * du_dxi * dxi_dx;
-    //             }
-    //         }
-    //     }
-    //     // global assembly
-    //     // distribute the entries to the global stiffness matrix and global load vector
-    //     for (int aa = 0; aa < user->n_en; ++aa){
-    //         int PP = user->ID[ user->IEN[aa * user->nElem + ee] - 1 ];
-    //         if (PP > 0){
-    //             // F[PP - 1] += f_ele[aa];
-    //             for (int bb = 0; bb < user->n_en; ++bb){
-    //                 int QQ = user->ID[ user->IEN[bb * user->nElem + ee] - 1 ];
-    //                 if (QQ > 0){
-    //                     K[(PP - 1) * user->n_eq + QQ - 1] += k_ele[aa * user->n_en + bb];
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // Modify the load vector by the Natural BC
-    //     // Note: for multi-dimensional cases, one needs to perform line or
-    //     // surface integration for the natural BC.
-    //     // if (ee == 0){
-    //     //     F[ user->ID[ user->IEN[0] - 1 ] - 1 ] += h( user->x_coor[ user->IEN[0] - 1 ]);
-    //     // }
+                for (int bb = 0; bb < user->n_en; ++bb){
+                    double Nb = 0.0;
+                    PolyBasis(user->pp, bb + 1, 0, user->qp[ll], Nb);
+                    double Nb_xi = 0.0;
+                    PolyBasis(user->pp, bb + 1, 1, user->qp[ll], Nb_xi);
+                    k_ele[aa * user->n_en + bb] += user->wq[ll] * Na_xi * kappa * Nb_xi * dxi_dx;
+                    k_ele[aa * user->n_en + bb] += user->wq[ll] * Na_xi * dkappa * Nb * du_dxi * dxi_dx;
+                }
+            }
+        }
+        // global assembly
+        // distribute the entries to the global stiffness matrix and global load vector
+        for (int aa = 0; aa < user->n_en; ++aa){
+            int PP = user->ID[ user->IEN[aa * user->nElem + ee] - 1 ];
+            if (PP > 0){
+                for (int bb = 0; bb < user->n_en; ++bb){
+                    int QQ = user->ID[ user->IEN[bb * user->nElem + ee] - 1 ];
+                    if (QQ > 0){
+                        K[(PP - 1) * user->n_eq + QQ - 1] += k_ele[aa * user->n_en + bb];
+                    }
+                }
+            }
+        }
         
-    // } //  end of element loop
+    } //  end of element loop
 
     std::cout << "K: ";
     for (int ii = 0; ii < user->n_eq * user->n_eq; ++ii){
         if (ii % user->n_eq == 0) std::cout << std::endl;
-        std::cout << user->K[ii] << "\t";
+        std::cout << K[ii] << "\t";
     }
     std::cout << std::endl;
 
     for (PetscInt ii = 0; ii < user->n_eq; ++ii){
         for (PetscInt jj = 0; jj < user->n_eq; ++jj){
-            ierr = MatSetValues(B, 1, &ii, 1, &jj, &user->K[ii * user->n_eq + jj], INSERT_VALUES); CHKERRQ(ierr);
+            ierr = MatSetValues(B, 1, &ii, 1, &jj, &K[ii * user->n_eq + jj], INSERT_VALUES); CHKERRQ(ierr);
         }
     }
         
